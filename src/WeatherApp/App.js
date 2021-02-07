e157d85ed4ccce73606a4550a9a88b9c6c035dbd
DaniilGit <axsell232@gmail.com>
2021-02-07 18:34:06 +0700

    Stable version

--- src/WeatherApp/App.js
+++ src/WeatherApp/App.js
@@ -1,28 +1,28 @@
-import React, {useEffect} from "react";
+import React, {Component, useEffect, useState} from "react";
+import ReactDOM from "react-dom";
 import axios from "axios";
-import Content from "./Content";
 import getWeatherNextDays from "./function/getWeatherNextDays";
-import Loader from "./Loader";
+import Content from "./Content";
 
 async function getWeather(nameCity, setData, data) {
-  // const response = await axios.get(
-  //   `https://api.openweathermap.org/data/2.5/forecast?q=${nameCity}&lang=ru&units=metric&appid=e6ebd56804d0ed1639181c4a36c41c1c`
-  // );
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${nameCity}&lang=ru&units=metric&appid=e6ebd56804d0ed1639181c4a36c41c1c`
+  );
 
-  // let city = {
-  //   name: response.data.city.name,
-  //   currentDay: {
-  //     temp: response.data.list[0].main.temp,
-  //     tempMax: response.data.list[0].main.temp_max,
-  //     tempMin: response.data.list[0].main.temp_min,
-  //     clouds: response.data.list[0].weather[0].description,
-  //     wind: response.data.list[0].wind.speed,
-  //     humidity: response.data.list[0].main.humidity,
-  //   },
-  //   nextDays: getWeatherNextDays(response.data.list),
-  // };
+  let city = {
+    name: response.data.city.name,
+    currentDay: {
+      temp: response.data.list[0].main.temp,
+      tempMax: response.data.list[0].main.temp_max,
+      tempMin: response.data.list[0].main.temp_min,
+      clouds: response.data.list[0].weather[0].description,
+      wind: response.data.list[0].wind.speed,
+      humidity: response.data.list[0].main.humidity,
+    },
+    nextDays: getWeatherNextDays(response.data.list),
+  };
 
-  setData({...data, newCity: "HI"});
+  setData({...data, newCity: city});
 }
 
 function App(props) {
@@ -33,15 +33,16 @@ function App(props) {
   const [isLoading, setLoading] = React.useState(false);
 
   useEffect(() => {
-    getWeather("moscow", setData, data);
+    getWeather("omsk", setData, data);
     setLoading(true);
-  });
+  }, []);
 
   console.log(data.newCity);
-  if (!isLoading)
-    return <div>Загрузка</div>;
-  else
-    return <Content isLoading={isLoading} />;
+  if (!isLoading) {
+    return <div>Загрузка!</div>;
+  } else {
+    return <Content />
+  }
 }
 
 export default App;
