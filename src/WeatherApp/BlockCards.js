67e0e72a9aac48d2290f813e56303feca624b03a
DaniilGit <axsell232@gmail.com>
2021-02-12 13:16:38 +0700

    Add animations

--- src/WeatherApp/BlockCards.js
+++ src/WeatherApp/BlockCards.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/Cards.css";
 import Card from "./Card";
 import AddCard from "./AddCard";
-
+import "../../node_modules/animate.css/animate.css";
 
 function BlockCards(props) {
   let cities = props.cities;
--- src/WeatherApp/ModalDetailed.js
+++ src/WeatherApp/ModalDetailed.js
@@ -1,6 +1,7 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcons";
 import "../styles/Modal.css";
+import "../../node_modules/animate.css/animate.css";
 
 function NextDay(props) {
   let day = props.day;
@@ -22,35 +23,36 @@ function ModalDetailed(props) {
     console.log(city);
     return (
       <div className="container-modal">
-        <div className="button-back" onClick={() => setModal({...props.modal, isOpen: false})}>
-          <svg
-            viewBox="4085 152 98.5 126"
-          >
+        <div
+          className="button-back"
+          onClick={() => setModal({...props.modal, isOpen: false})}
+        >
+          <svg viewBox="4085 152 98.5 126">
             <g transform="translate(3980)">
-            <circle
-              className="a"
-              cx="39"
-              cy="39"
-              r="39"
-              transform="translate(105 152)"
-            ></circle>
-            <line
-              className="b"
-              transform="translate(123.5 190.5)"
-              x1="80"
-            ></line>
-            <line
-              className="b"
-              transform="translate(123.5 164.5)"
-              x2="26"
-              y1="26"
-            ></line>
-            <line
-              className="c"
-              transform="translate(123.5 190.5)"
-              x1="26"
-              y1="27"
-            ></line>
+              <circle
+                className="a"
+                cx="39"
+                cy="39"
+                r="39"
+                transform="translate(105 152)"
+              ></circle>
+              <line
+                className="b"
+                transform="translate(123.5 190.5)"
+                x1="80"
+              ></line>
+              <line
+                className="b"
+                transform="translate(123.5 164.5)"
+                x2="26"
+                y1="26"
+              ></line>
+              <line
+                className="c"
+                transform="translate(123.5 190.5)"
+                x1="26"
+                y1="27"
+              ></line>
             </g>
           </svg>
         </div>
